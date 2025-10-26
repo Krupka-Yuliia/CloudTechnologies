c94d18942c7d0fbcd94378bdb10e16cba6e44555
Krupka-Yuliia <yuliia.krupka.22@pnu.edu.ua>
2025-10-26 11:36:32 +0200

    fix lab 10

--- lab10/outputs.tf
+++ lab10/outputs.tf
@@ -1,15 +1,14 @@
-output "vm_name" {
-  value = azurerm_windows_virtual_machine.vm.name
+output "vm_id" {
+  value       = azurerm_windows_virtual_machine.vm.id
+  description = "Virtual machine ID"
 }
 
-output "recovery_vault_region1" {
-  value = azurerm_recovery_services_vault.rsv_region1.name
+output "vault_id_region1" {
+  value       = azurerm_recovery_services_vault.rsv_rg1.id
+  description = "Recovery Services Vault ID (East US)"
 }
 
-output "recovery_vault_region2" {
-  value = azurerm_recovery_services_vault.rsv_region2.name
-}
-
-output "storage_account_diagnostics" {
-  value = azurerm_storage_account.diagnostics.name
+output "vault_id_region2" {
+  value       = azurerm_recovery_services_vault.rsv_rg2.id
+  description = "Recovery Services Vault ID (WestUS)"
 }
--- lab10/variables.tf
+++ lab10/variables.tf
@@ -1,29 +1,50 @@
-variable "rg_region1_name" {
-  default = "az104-rg-region1"
-}
-
-variable "rg_region2_name" {
-  default = "az104-rg-region2"
-}
-
 variable "subscription_id" {
   description = "Azure Subscription ID"
   type        = string
 }
 
-variable "admin_username" {
-  default = "localadmin"
-}
-
-variable "admin_password" {
-  sensitive = true
-  type      = string
-}
-
 variable "region1" {
-  default = "West Europe"
+  default     = "Central US"
+  description = "Primary region"
 }
 
 variable "region2" {
-  default = "West US"
+  default     = "West US"
+  description = "Secondary region for disaster recovery"
+}
+
+variable "resource_group_region1" {
+  default     = "az104-rg-region1"
+  description = "Resource group in East US"
+}
+
+variable "resource_group_region2" {
+  default     = "az104-rg-region2"
+  description = "Resource group in West US"
+}
+
+
+variable "admin_username" {
+  default     = "localadmin"
+  description = "Admin username for the VM"
+}
+
+variable "admin_password" {
+  description = "Admin password for the VM"
+  sensitive   = true
+}
+
+variable "rsv_region1" {
+  default     = "az104-rsv-region1"
+  description = "Recovery Services Vault in East US"
+}
+
+variable "rsv_region2" {
+  default     = "az104-rsv-region2"
+  description = "Recovery Services Vault in West US"
+}
+
+variable "tenant_id" {
+  description = "Azure Tenant ID"
+  type        = string
 }
